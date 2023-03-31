76fb55dc98ca5f672d856b792a84f2c7b038b87b
hasnainriazkayanii <70016622+hasnainriazkayanii@users.noreply.github.com>
2023-03-31 22:58:36 +0500

    ss

--- send-sms.php
+++ send-sms.php
@@ -1,7 +1,7 @@
 <?php
 require_once "libraries/vendor/autoload.php";
-$account_sid = 'AC16f42580ae3d7630ea8f25818dae8bf0';
-$auth_token = 'fa3a7301b02a9b4d1270040ad65b18a3';
+$account_sid = '';
+$auth_token = '';
 
 
 $twilio_number = "+14094077660";
